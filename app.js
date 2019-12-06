d732f380788474b5e91a8475382ade05b1fc9e69
Millione Johnson <mjohnson030@west-mec.org>
2019-12-06 15:29:30 -0700

    Batwoman

---
+++ app.js
@@ -0,0 +1,12 @@
+app.get('', (req, res) => {
+    res.render('index')
+return 	
+});
+
+//res ==  response aka what we send to the client
+//async tag on callback function allows 
+
+app.get('/pokemon/:id', (req, res) => {
+return 	
+});
+
